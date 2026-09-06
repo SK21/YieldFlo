bb7ec225829509fdcf36f736f29d24953deda280
SK21 <SK21@users.noreply.github.com>
2026-09-05 20:55:55 -0600

    Update Props.cs

--- YieldFlo/Classes/Props.cs
+++ YieldFlo/Classes/Props.cs
@@ -11,6 +11,7 @@ namespace YieldFlo.Classes
     {
         public static readonly string AppName = "YieldFlo";
         public static readonly string AppVersion = "1.0.1 - beta 1.2";
+        public static readonly string AppDate = "5-Sep-2026";
 
         private static string cApplicationFolder;
         private static string cDataFolder;
